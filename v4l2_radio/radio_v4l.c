f11c1002b04e848d49688d48d6cb6a2a3026a9d8
Nickolay Semendyaev <agent00791@gmail.com>
2016-09-10 01:45:02 +0600

    FM: add speaker mixer

--- v4l2_radio/radio_v4l.c
+++ v4l2_radio/radio_v4l.c
@@ -22,6 +22,7 @@
 #include <tinyalsa/asoundlib.h>
 #include "include/fmradio.h"
 #include "include/v4l2_ioctl.h"
+#include <audio_route/audio_route.h>
 
 #define INFO(fmt, ...) fprintf(stderr, "[INFO] " fmt, ##__VA_ARGS__)
 #define ERR(fmt, ...) fprintf(stderr, "[ERR]  " fmt, ##__VA_ARGS__)
@@ -33,109 +34,231 @@ static int fd = -1;
 #define SLIMBUS_DL_HL_Switch "SLIMBUS_DL_HL Switch"
 #define SLIM_0_RX_Format "SLIM_0_RX Format"
 #define SLIM_0_RX_SampleRate "SLIM_0_RX SampleRate"
+//Sony speaker
+/*
+    <path name="speaker">
+        <ctl name="AIF4_VI Mixer SPKR_VI_1" value="1" />
+        <ctl name="AIF4_VI Mixer SPKR_VI_2" value="1" />
+        <ctl name="SLIM RX1 MUX" value="AIF1_PB" />
+        <ctl name="SLIM RX2 MUX" value="AIF1_PB" />
+        <ctl name="SLIM_0_RX Channels" value="Two" />
+        <ctl name="RX7 MIX1 INP1" value="RX1" />
+        <ctl name="RX8 MIX1 INP1" value="RX2" />
+        <ctl name="RX7 Digital Volume" value="84" />
+        <ctl name="RX8 Digital Volume" value="84" />
+        <ctl name="COMP0 Switch" value="1" />
+        <ctl name="VI_FEED_TX Channels" value="Two" />
+        <ctl name="SLIM0_RX_VI_FB_LCH_MUX"  value="SLIM4_TX" />
+        <ctl name="SLIM0_RX_VI_FB_RCH_MUX"  value="SLIM4_TX" />
+    </path>
+*/
+#define AIF4_VI_Mixer_SPKR_VI_1 "AIF4_VI Mixer SPKR_VI_1"
+#define AIF4_VI_Mixer_SPKR_VI_2 "AIF4_VI Mixer SPKR_VI_2"
+#define SLIM_RX1_MUX "SLIM RX1 MUX"
+#define SLIM_RX2_MUX "SLIM RX2 MUX"
+#define SLIM_0_RX_Channels "SLIM_0_RX Channels"
+#define RX7_MIX1_INP1 "RX7 MIX1 INP1"
+#define RX8_MIX1_INP1 "RX8 MIX1 INP1"
+#define RX7_Digital_Volume "RX7 Digital Volume"
+#define RX8_Digital_Volume "RX8 Digital Volume"
+#define COMP0_Switch "COMP0 Switch"
+#define VI_FEED_TX_Channels "VI_FEED_TX Channels"
+#define SLIM0_RX_VI_FB_LCH_MUX "SLIM0_RX_VI_FB_LCH_MUX"
+#define SLIM0_RX_VI_FB_RCH_MUX "SLIM0_RX_VI_FB_RCH_MUX"
+
+#define DEFAULT_OUTPUT_SAMPLING_RATE 44000
+#define LOW_LATENCY_OUTPUT_PERIOD_SIZE 240
+#define LOW_LATENCY_OUTPUT_PERIOD_COUNT 2
+
+int setMixerBoolCtl(char* name, bool val) {
+    int ret = 0;
+    enum mixer_ctl_type type;
+    struct mixer_ctl *ctl;
+    struct mixer *mixer = mixer_open(0);
+    ctl = mixer_get_ctl_by_name(mixer, name);
+    if (ctl == NULL) {
+        mixer_close(mixer);
+        ERR("%s: Could not find %s\n", __func__, name);
+        return -ENODEV;
+    }
+
+    type = mixer_ctl_get_type(ctl);
+    if (type != MIXER_CTL_TYPE_BOOL) {
+        ERR("%s: %s is not supported\n", __func__, name);
+        mixer_close(mixer);
+        return -ENOTTY;
+    }
+    ret = mixer_ctl_set_value(ctl, 0, val);
+    if (ret < 0)
+        return -ENOTTY;
+
+    mixer_close(mixer);
+    return 0;
+}
+
+int setMixerIntCtl(char* name, bool val) {
+    int ret = 0;
+    enum mixer_ctl_type type;
+    struct mixer_ctl *ctl;
+    struct mixer *mixer = mixer_open(0);
+    ctl = mixer_get_ctl_by_name(mixer, name);
+    if (ctl == NULL) {
+        mixer_close(mixer);
+        ERR("%s: Could not find %s\n", __func__, name);
+        return -ENODEV;
+    }
+
+    type = mixer_ctl_get_type(ctl);
+    if (type != MIXER_CTL_TYPE_INT) {
+        ERR("%s: %s is not supported\n", __func__, name);
+        mixer_close(mixer);
+        return -ENOTTY;
+    }
+    ret = mixer_ctl_set_value(ctl, 0, val);
+    if (ret < 0)
+        return -ENOTTY;
+
+    mixer_close(mixer);
+    return 0;
+}
+
+
+int setMixerEnumNameCtl(char* name, char* val) {
+    int ret = 0;
+    enum mixer_ctl_type type;
+    struct mixer_ctl *ctl;
+    struct mixer *mixer = mixer_open(0);
+    ctl = mixer_get_ctl_by_name(mixer, name);
+    if (ctl == NULL) {
+        mixer_close(mixer);
+        ERR("%s: Could not find %s\n", __func__, name);
+        return -ENODEV;
+    }
+
+    type = mixer_ctl_get_type(ctl);
+    if (type != MIXER_CTL_TYPE_ENUM) {
+        ERR("%s: %s is not supported, %d\n", __func__, name, type);
+        mixer_close(mixer);
+        return -ENOTTY;
+    }
+    ret = mixer_ctl_set_enum_by_string(ctl, val);
+    if (ret < 0)
+        return -ENOTTY;
+
+    mixer_close(mixer);
+    return 0;
+}
+
 static int initMixer() {
     int ret = 0;
     enum mixer_ctl_type type;
     struct mixer_ctl *ctl;
     struct mixer *mixer = mixer_open(0);
+    unsigned int flags = PCM_OUT;
 
     if (mixer == NULL) {
         ERR("Error opening mixer 0");
         return -1;
     }
 
+//speaker
+//AIF4_VI Mixer SPKR_VI_1
+    ret = setMixerBoolCtl(AIF4_VI_Mixer_SPKR_VI_1, true);
+    if (ret < 0)
+        return -1;
+//AIF4_VI Mixer SPKR_VI_2
+    ret = setMixerBoolCtl(AIF4_VI_Mixer_SPKR_VI_2, true);
+    if (ret < 0)
+        return -1;
+//SLIM RX1 MUX
+    ret = setMixerEnumNameCtl(SLIM_RX1_MUX, "AIF1_PB");
+    if (ret < 0)
+        return -1;
+//SLIM RX2 MUX
+    ret = setMixerEnumNameCtl(SLIM_RX2_MUX, "AIF1_PB");
+    if (ret < 0)
+        return -1;
+//SLIM_0_RX Channels
+    ret = setMixerEnumNameCtl(SLIM_0_RX_Channels, "Two");
+    if (ret < 0)
+        return -1;
+//RX7_MIX1_INP1
+    ret = setMixerEnumNameCtl(RX7_MIX1_INP1, "RX1");
+    if (ret < 0)
+        return -1;
+//RX8_MIX1_INP1
+    ret = setMixerEnumNameCtl(RX8_MIX1_INP1, "RX2");
+    if (ret < 0)
+        return -1;
+//RX7 Digital Volume
+    ret = setMixerIntCtl(RX7_Digital_Volume, 84);
+    if (ret < 0)
+        return -1;
+//RX8 Digital Volume
+    ret = setMixerIntCtl(RX8_Digital_Volume, 84);
+    if (ret < 0)
+        return -1;
+//COMP0 Switch
+    ret = setMixerBoolCtl(COMP0_Switch, true);
+    if (ret < 0)
+        return -1;
+//VI_FEED_TX Channels
+    ret = setMixerEnumNameCtl(VI_FEED_TX_Channels, "Two");
+    if (ret < 0)
+        return -1;
+//SLIM0_RX_VI_FB_LCH_MUX
+    ret = setMixerEnumNameCtl(SLIM0_RX_VI_FB_LCH_MUX, "SLIM4_TX");
+    if (ret < 0)
+        return -1;
+//SLIM0_RX_VI_FB_RCH_MUX
+    ret = setMixerEnumNameCtl(SLIM0_RX_VI_FB_RCH_MUX, "SLIM4_TX");
+    if (ret < 0)
+        return -1;
+
+//play-fm
 //SLIM_0_RX_Format
-    ctl = mixer_get_ctl_by_name(mixer, SLIM_0_RX_Format);
-    if (ctl == NULL) {
-        mixer_close(mixer);
-        ERR("%s: Could not find %s\n", __func__, SLIM_0_RX_Format);
-        return -ENODEV;
-    }
-
-    type = mixer_ctl_get_type(ctl);
-    if (type != MIXER_CTL_TYPE_ENUM) {
-        ERR("%s: %s is not supported, %d\n", __func__, SLIM_0_RX_Format, type);
-        mixer_close(mixer);
-        return -ENOTTY;
-    }
-    ret = mixer_ctl_set_enum_by_string(ctl, "S16_LE");
+    ret = setMixerEnumNameCtl(SLIM_0_RX_Format, "S16_LE");
     if (ret < 0)
         return -1;
-
-
 //SLIM_0_RX_SampleRate
-    ctl = mixer_get_ctl_by_name(mixer, SLIM_0_RX_SampleRate);
-    if (ctl == NULL) {
-        mixer_close(mixer);
-        ERR("%s: Could not find %s\n", __func__, SLIM_0_RX_SampleRate);
-        return -ENODEV;
-    }
-
-    type = mixer_ctl_get_type(ctl);
-    if (type != MIXER_CTL_TYPE_ENUM) {
-        ERR("%s: %s is not supported, %d\n", __func__, SLIM_0_RX_SampleRate, type);
-        mixer_close(mixer);
-        return -ENOTTY;
-    }
-    ret = mixer_ctl_set_enum_by_string(ctl, "KHZ_48");
+    ret = setMixerEnumNameCtl(SLIM_0_RX_SampleRate, "KHZ_48");
     if (ret < 0)
         return -1;
-
 //PRI_MI2S_LOOPBACK_Volume
-    ctl = mixer_get_ctl_by_name(mixer, PRI_MI2S_LOOPBACK_Volume);
-    if (ctl == NULL) {
-        mixer_close(mixer);
-        ERR("%s: Could not find %s\n", __func__, PRI_MI2S_LOOPBACK_Volume);
-        return -ENODEV;
-    }
-
-    type = mixer_ctl_get_type(ctl);
-    if (type != MIXER_CTL_TYPE_INT) {
-        ERR("%s: %s is not supported\n", __func__, PRI_MI2S_LOOPBACK_Volume);
-        mixer_close(mixer);
-        return -ENOTTY;
-    }
-    ret = mixer_ctl_set_value(ctl, 0, 1);
+    ret = setMixerIntCtl(PRI_MI2S_LOOPBACK_Volume, 1);
     if (ret < 0)
         return -1;
-
 //SLIMBUS_0_RX_Port_Mixer_PRI_MI2S_TX
-    ctl = mixer_get_ctl_by_name(mixer, SLIMBUS_0_RX_Port_Mixer_PRI_MI2S_TX);
-    if (ctl == NULL) {
-        mixer_close(mixer);
-        ERR("%s: Could not find %s\n", __func__, SLIMBUS_0_RX_Port_Mixer_PRI_MI2S_TX);
-        return -ENODEV;
-    }
-
-    type = mixer_ctl_get_type(ctl);
-    if (type != MIXER_CTL_TYPE_BOOL) {
-        ERR("%s: %s is not supported\n", __func__, SLIMBUS_0_RX_Port_Mixer_PRI_MI2S_TX);
-        mixer_close(mixer);
-        return -ENOTTY;
-    }
-    ret = mixer_ctl_set_value(ctl, 0, 1);
+    ret = setMixerBoolCtl(SLIMBUS_0_RX_Port_Mixer_PRI_MI2S_TX, true);
     if (ret < 0)
         return -1;
-
 //SLIMBUS_DL_HL_Switch
-    ctl = mixer_get_ctl_by_name(mixer, SLIMBUS_DL_HL_Switch);
-    if (ctl == NULL) {
-        mixer_close(mixer);
-        ERR("%s: Could not find %s\n", __func__, SLIMBUS_DL_HL_Switch);
-        return -ENODEV;
-    }
-
-    type = mixer_ctl_get_type(ctl);
-    if (type != MIXER_CTL_TYPE_BOOL) {
-        ERR("%s: %s is not supported\n", __func__, SLIMBUS_DL_HL_Switch);
-        mixer_close(mixer);
-        return -ENOTTY;
-    }
-    ret = mixer_ctl_set_value(ctl, 0, 1);
+    ret = setMixerBoolCtl(SLIMBUS_DL_HL_Switch, true);
     if (ret < 0)
         return -1;
 
-    mixer_close(mixer);
+
+    struct pcm *pcm;
+    struct pcm_config config = {  //From audio hal
+                .channels = 2,
+                .rate = DEFAULT_OUTPUT_SAMPLING_RATE,
+                .period_size = LOW_LATENCY_OUTPUT_PERIOD_SIZE,
+                .period_count = LOW_LATENCY_OUTPUT_PERIOD_COUNT,
+                .format = PCM_FORMAT_S16_LE,
+                .start_threshold = LOW_LATENCY_OUTPUT_PERIOD_SIZE / 4,
+                .stop_threshold = INT_MAX,
+                .avail_min = LOW_LATENCY_OUTPUT_PERIOD_SIZE / 4,
+    };
+
+    flags |= PCM_MONOTONIC; //From audiohal
+
+/*
+    pcm = pcm_open(0, 1, flags, &config); //From audiohal
+    if (!pcm || !pcm_is_ready(pcm)) {
+          ERR("pcm_open failed: %s\n", pcm_get_error(pcm));
+          return -1;
+    }
+*/
 
     return 0;
 }
@@ -176,8 +299,8 @@ static int open_device()
         return -1;
     INFO("End set audmode stereo\n");
 
-    INFO("Start set freq: %.6f\n", 103900*fact);
-    ret = set_freq(fd, 103900*fact);
+    INFO("Start set freq: %.6f\n", 104400*fact);
+    ret = set_freq(fd, 104400*fact);
     if (ret < 0)
         return -1;
     INFO("End set freq\n");
